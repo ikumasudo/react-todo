e672137f3a71d39aa58042991209e03e07342be0
ikumasudo <adgjmptw.montblanc@gmail.com>
2024-11-20 17:01:15 +0900

    react + pocketbase setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react"
+import pb from "./lib/pb"
+
 function App() {
-  return <h1>Hello world!</h1>
+  const [msg, setMsg] = useState("")
+
+  useEffect(() => {
+    async function healthCheck() {
+      const res = await pb.health.check()
+      console.log(res)
+      setMsg(res.message)
+    }
+    healthCheck()
+  }, [])
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>message from pocketbase: {msg}</p>
+    </>
+  )
 }
 
 export default App
