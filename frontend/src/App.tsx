9e93c5f53034f8ee864f6e979d0e787f3b222be3
ikumasudo <adgjmptw.montblanc@gmail.com>
2024-11-20 17:31:51 +0900

    Todo作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,11 +11,32 @@ interface Todo {
 function App() {
   const [todos, setTodos] = useState<Todo[]>([])
 
+  async function fetchTodos() {
+    const res = await pb.collection("todo").getFullList<Todo>()
+    setTodos(res)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const data = {
+      title: formData.get('title'),
+      description: formData.get('description'),
+    };
+
+    await pb.collection("todo").create({
+      title: data.title,
+      completed: false,
+      description: data.description
+    })
+
+    await fetchTodos()
+    form.reset()
+  };
+
   useEffect(() => {
-    async function fetchTodos() {
-      const res = await pb.collection("todo").getFullList<Todo>()
-      setTodos(res)
-    }
     fetchTodos()
   }, [])
 
@@ -31,6 +52,12 @@ function App() {
           </li>
         ))}
       </ol>
+
+      <form onSubmit={handleSubmit}>
+        <input name="title" />
+        <input name="description" />
+        <button type="submit">追加</button>
+      </form>
     </>
   )
 }
